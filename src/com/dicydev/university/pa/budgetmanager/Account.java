a6554c40cff4cdfc7ddb864ca4d22fd32c3eb9e5
Lorenzo Lapucci <halexercraft@gmail.com>
2022-03-30 16:03:46 +0200

    Add one more method

--- src/com/dicydev/university/pa/budgetmanager/Account.java
+++ src/com/dicydev/university/pa/budgetmanager/Account.java
@@ -1,8 +1,12 @@
 package com.dicydev.university.pa.budgetmanager;
 
+import com.dicydev.university.pa.budgetmanager.transaction.Transaction;
+
 /**
  * Transactions in the ledger are linked to a specific account.
  */
 public interface Account {
     double getBalance();
+
+    void addTransaction(Transaction transaction);
 }
