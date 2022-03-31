2e703064c423e97c4a50f67cb39a47ebab940492
Lorenzo Lapucci <halexercraft@gmail.com>
2022-03-31 10:59:53 +0200

    Change Java language version to 17 and run some tests in Main

--- src/com/dicydev/university/pa/budgetmanager/Main.java
+++ src/com/dicydev/university/pa/budgetmanager/Main.java
@@ -1,7 +1,36 @@
 package com.dicydev.university.pa.budgetmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        //Integer[] n = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //set0(n, 10d);
 
+        ArrayList<Number> numbers = new ArrayList<>();
+        numbers.add(Integer.valueOf(1));
+        numbers.add(Double.valueOf(2));
+        add(numbers, 3f);
+
+        for (Number number : numbers) {
+            if (number instanceof Integer) {
+                System.out.println("Integer");
+            } else if (number instanceof Double) {
+                System.out.println("Double");
+            } else if (number instanceof Float) {
+                System.out.println("Float");
+            } else {
+                throw new IllegalStateException("Unexpected value: " + number);
+            }
+        }
+    }
+
+    private static void set0(Number[] n, Number newValue) {
+        n[0] = newValue;
+    }
+
+    private static void add(List<Number> n, Number newValue) {
+        n.add(newValue);
     }
 }
